5ef97d49c4cfaa4df30c92d9542458440ce93ca9
mitjap <mitja.potocin@innovatif.com>
2020-03-03 08:53:50 +0100

    Fixed untracked files

---
+++ functions.js
@@ -0,0 +1,15 @@
+Letters.init({
+	selector: '.container',
+	lettersOver: []
+});
+
+Letters.init({
+	selector: '.container2',
+	lettersOver: [1, 2, 3, 4]
+});
+
+Letters.init({
+	selector: '.container3'
+});
+
+Letters.init();
